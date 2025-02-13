76c4669a7649cbc7568d5ce9fd297cbc1ed24139
Josh Bañon <joshuabanonvalera@hotmail.com>
2025-02-13 01:54:49 +0100

    Correct horusec error

--- CodeAnalizer/CodeAnalizer/static/js/title.js
+++ CodeAnalizer/CodeAnalizer/static/js/title.js
@@ -19,6 +19,15 @@ function stopTitleAnimation() {
 }
 
 document.addEventListener("visibilitychange", function () {
+    const trustedOrigin = "https://my-app.com";
+
+    window.addEventListener("message", (event) => {
+        if (event.origin !== trustedOrigin) {
+            console.error("Untrusted origin:", event.origin);
+            return;
+        }
+    });
+    
     if (!document.hidden) {
         // User returns to the tab
         document.title = last_title;
